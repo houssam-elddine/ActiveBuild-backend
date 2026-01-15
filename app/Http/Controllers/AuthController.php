35d4c575c2de08ebbcd3de5bec3210c819386553
houssam-elddine <vortixdev@gmail.com>
2026-01-15 19:26:25 +0100

    feat : Auth Controller and Route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'data' => $user,
+            ]);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Deconnexion réussie']);
+    }
+}
